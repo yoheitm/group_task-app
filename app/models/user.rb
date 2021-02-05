08e583a133e217633003988e2d458615566dbd4c
yoheitm <tmyoyy9617@gmail.com>
2021-02-05 18:33:55 +0900

    ユーザー情報をテーブルへ保存する機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :name, :email
+    with_options format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ } do
+      validates :password
+      validates :password_confirmation
+    end
+  end
 end
