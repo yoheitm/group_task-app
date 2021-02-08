c6ae5869d52eba2a77906ec2ab4b400b2f5b2bd9
yoheitm <tmyoyy9617@gmail.com>
2021-02-08 21:50:25 +0900

    グループ作成機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,17 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
 end
