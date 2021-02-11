48eb22ef2e27dcb35aa9bc415bd11f36a8a8d04a
yoheitm <tmyoyy9617@gmail.com>
2021-02-11 19:10:15 +0900

    入力エラー時のレイアウト崩れの解消

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,12 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
   def create
     @group = Group.new(group_params)
-    if @group.valid?
-      @group.save
+    if @group.save
       redirect_to root_path
     else
       render :new
@@ -15,6 +15,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, :user_ids[])
+    params.require(:group).permit(:name, user_ids:[])
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
 
   validates :name, presence: true
+  validates :user_ids, presence: true
 end
