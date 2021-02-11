01b98091891ecee4c99d2bf26b07c48bdb98a9af
yoheitm <tmyoyy9617@gmail.com>
2021-02-11 20:47:56 +0900

    ユーザーアイコンのCSS調整

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [200, 200]
   end
   version :thumb50 do
-    process resize_to_fit: [40, 40]
+    process resize_to_fit: [30, 30]
   end
   # Choose what kind of storage to use for this uploader:
   storage :file
