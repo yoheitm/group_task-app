2129c1662d6381fec5837114aee8a6bcd5f93994
yoheitm <tmyoyy9617@gmail.com>
2021-02-11 20:37:49 +0900

    ユーザーアイコンのプルダウンの実装

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,12 +1,18 @@
 function pull (){
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
-  pullDownButton.addEventListener('click', function(){
+  pullDownButton.addEventListener('click', () => {
     if (pullDownParents.getAttribute("style") == "display:block;") {
       pullDownParents.removeAttribute("style", "display:block;")
     } else {
       pullDownParents.setAttribute("style", "display:block;")
     }
+  });
+  pullDownButton.addEventListener("mouseover", () => {
+    pullDownButton.setAttribute("style", "background-color:rgb(0, 95, 48);")
+  });
+  pullDownButton.addEventListener("mouseout", () => {
+    pullDownButton.removeAttribute("style", "background-color:rgb(0, 95, 48);")
   });
 }
 
