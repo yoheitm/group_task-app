1e94fb704922b39a67872a71288401f1b3b40c9f
yoheitm <tmyoyy9617@gmail.com>
2021-02-12 16:05:38 +0900

    グループ名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to group_messages_path(@group)
     else
+      @messages = @group.messages.includes(:user)
       render :index
     end
   end
@@ -17,6 +19,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:dontent).merge(user_id: current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
